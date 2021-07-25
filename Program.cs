f151ba079f39e5e146ade28908d79a5b265e784e
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-07-25 13:38:07 +0530

    [Ankit] Add : Checks whether both lines are equal or not

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ namespace Line_Comparison
     {
         static void Main(string[] args)
         {
-            Line line = new Line(0, 2, 4, 8);
-            line.Calculate();
+            Line line1 = new Line(0, 2, 4, 8);
+            Line line2 = new Line(8, 10, 12, 14);
+
+            line1.Calculate();
+            line2.Calculate();
+            if (line1.Equals(line2))
+                Console.WriteLine("Both lines are of same size");
+            else
+                Console.WriteLine("Both lines are of different sizes");
         }
     }
 }
